9a081f803908ee95b9eb9e2619a779d6aa4cc218
Aditya Mishra <mishraaaditya29@gmail.com>
2025-09-23 11:51:28 +0530

    remove unused LayerNormalization class from model.py

--- Transformer/model.py
+++ Transformer/model.py
@@ -1,9 +1,3 @@
 import math
 import torch
 import torch.nn as nn
-
-
-class LayerNormalization(nn.Module):
-
-    def __init__(self, features: int, eps: float = 10**-6 -> None):
-        super().__init__()  # Initialize the LayerNormalization module
