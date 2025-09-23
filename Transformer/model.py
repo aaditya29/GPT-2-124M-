1dfb51989c32003764ccd7025aba2ee797024f80
Aditya Mishra <mishraaaditya29@gmail.com>
2025-09-23 18:41:10 +0530

    added InputEmbeddings class for embedding layer in model.py

--- Transformer/model.py
+++ Transformer/model.py
@@ -1,3 +1,17 @@
 import math
 import torch
 import torch.nn as nn
+
+
+class InputEmbeddings(nn.Module):
+
+    def __init__(self, d_model: int, vocab_size: int) -> None:  # (d_model, vocab_size)
+        super().__init__()  # Initialize the parent class
+        self.d_model = d_model  # Dimension of the model
+        self.vocab_size = vocab_size  # Size of the vocabulary
+        self.embedding = nn.Embedding(vocab_size, d_model)  # Embedding layer
+
+    def forward(self, x):
+        # (batch, seq_len) --> (batch, seq_len, d_model)
+        # Multiply by sqrt(d_model) to scale the embeddings according to the paper
+        return self.embedding(x) * math.sqrt(self.d_model)
