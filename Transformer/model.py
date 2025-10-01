69b3aeaf7d48341d194fe31e21fd04bf72be67f0
Aditya Mishra <mishraaaditya29@gmail.com>
2025-10-01 11:48:06 +0530

    added Encoder and DecoderBlock classes with forward methods and layer normalization

--- Transformer/model.py
+++ Transformer/model.py
@@ -186,3 +186,40 @@ class EncoderBlock(nn.Module):
         x = self.residual_connections[1](
             x, self.feed_forward_block)  # Feed-forward sub-layer
         return x
+
+
+class Encoder(nn.Module):
+
+    # nn.ModuleList[EncoderBlock]
+    def __init__(self, features: int, layers: nn.ModuleList) -> None:
+        super().__init__()  # Initialize the parent class
+        self.layers = layers  # List of encoder blocks
+        self.norm = LayerNormalization(features)  # Final layer normalization
+
+    def forward(self, x, mask):
+        for layer in self.layers:  # Iterate through each encoder block
+            # Pass the output of the previous block as input to the next block
+            x = layer(x, mask)
+        return self.norm(x)  # Apply final layer normalization
+
+
+class DecoderBlock(nn.Module):
+
+    def __init__(self, features: int, self_attention_block: MultiHeadAttentionBlock, cross_attention_block: MultiHeadAttentionBlock, feed_forward_block: FeedForwardBlock, dropout: float) -> None:
+
+        super().__init__()  # Initialize the parent class
+        # Masked multi-head attention block
+        self.self_attention_block = self_attention_block
+        self.cross_attention_block = cross_attention_block  # Multi-head attention block
+        self.feed_forward_block = feed_forward_block  # Feed-forward block
+        self.residual_connections = nn.ModuleList(
+            [ResidualConnection(features, dropout) for _ in range(3)])  # Three residual connections for the three sub-layers in the decoder block
+
+    def forward(self, x, encoder_output, src_mask, tgt_mask):
+        x = self.residual_connections[0](
+            x, lambda x: self.self_attention_block(x, x, x, tgt_mask))  # Masked self-attention sub-layer
+        x = self.residual_connections[1](x, lambda x: self.cross_attention_block(
+            x, encoder_output, encoder_output, src_mask))  # Cross-attention sub-layer
+        x = self.residual_connections[2](
+            x, self.feed_forward_block)  # Feed-forward sub-layer
+        return x
