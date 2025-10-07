1b3e06e3193ede4827b1e78463346bd4f36d0b90
Aditya Mishra <mishraaaditya29@gmail.com>
2025-10-07 15:16:58 +0530

    added greedy decoding function and training logic with model preloading support

--- Transformer/train.py
+++ Transformer/train.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader, random_split
 
 # for creating causal mask and dataset class
-from dataset import BilingualDataset, causal_mask
+from dataset import BilingualDataset, causal_mask, latest_weights_file_path, get_weights_file_path
 from model import build_transformer
 
 
@@ -14,7 +14,59 @@ from tokenizers.models import WordLevel  # for word-level tokenization
 from tokenizers.trainers import WordLevelTrainer
 from tokenizers.pre_tokenizers import Whitespace  # for whitespace tokenization
 
+
+import torchmetrics  # for evaluation metrics
+from torch.utils.tensorboard import SummaryWriter  # for tensorboard
+
+import warnings
+from tqdm import tqdm
+import os
 from pathlib import Path
+
+
+"""
+Here we are building greedy decoding function which will be used during inference to generate the target sentence given the source sentence using the trained transformer model and also functions to get dataset, build or load tokenizer and build the transformer model itself.
+model - trained transformer model
+source - source sentence tensor
+source_mask - mask for source sentence
+tokenizer_src - tokenizer for source language
+tokenizer_tgt - tokenizer for target language
+max_len - maximum length of the target sentence to be generated
+device - device to run the model on (cpu or gpu)
+"""
+
+
+def greedy_decode(model, source, source_mask, tokenizer_src, tokenizer_tgt, max_len, device):
+    sos_idx = tokenizer_tgt.token_to_id('[SOS]')
+    eos_idx = tokenizer_tgt.token_to_id('[EOS]')
+
+    # Precompute the encoder output and reuse it for every step
+    encoder_output = model.encode(source, source_mask)
+    # Initialize the decoder input with the sos token
+    decoder_input = torch.empty(1, 1).fill_(sos_idx).type_as(source).to(device)
+    while True:
+        if decoder_input.size(1) == max_len:
+            break
+
+        # build mask for target
+        decoder_mask = causal_mask(decoder_input.size(
+            1)).type_as(source_mask).to(device)
+
+        # calculate output
+        out = model.decode(encoder_output, source_mask,
+                           decoder_input, decoder_mask)
+
+        # get next token
+        prob = model.project(out[:, -1])
+        _, next_word = torch.max(prob, dim=1)
+        decoder_input = torch.cat(
+            [decoder_input, torch.empty(1, 1).type_as(source).fill_(next_word.item()).to(device)], dim=1
+        )
+
+        if next_word == eos_idx:
+            break
+
+    return decoder_input.squeeze(0)
 
 
 def get_all_sentences(ds, lang):
@@ -93,3 +145,97 @@ def get_model(config, vocab_src_len, vocab_tgt_len):
     model = build_transformer(vocab_src_len, vocab_tgt_len,
                               config["seq_len"], config['seq_len'], d_model=config['d_model'])
     return model
+
+
+"""
+def train_model(config):
+    device = "cuda" if torch.cuda.is_available(
+    ) else "mps" if torch.has_mps or torch.backends.mps.is_available() else "cpu"
+    print("Using device:", device)
+    Path(f"{config['datasource']}_{config['model_folder']}").mkdir(
+        parents=True, exist_ok=True)
+
+    train_dataloader, val_dataloader, tokenizer_src, tokenizer_tgt = get_ds(
+        config)
+    model = get_model(config, tokenizer_src.get_vocab_size(),
+                      tokenizer_tgt.get_vocab_size()).to(device)
+    # Tensorboard
+    writer = SummaryWriter(config['experiment_name'])
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=config['lr'], eps=1e-9)
+
+    # If the user specified a model to preload before training, load it
+    initial_epoch = 0
+    global_step = 0
+    preload = config['preload']
+    model_filename = latest_weights_file_path(
+        config) if preload == 'latest' else get_weights_file_path(config, preload) if preload else None
+    if model_filename:
+        print(f'Preloading model {model_filename}')
+        state = torch.load(model_filename)
+        model.load_state_dict(state['model_state_dict'])
+        initial_epoch = state['epoch'] + 1
+        optimizer.load_state_dict(state['optimizer_state_dict'])
+        global_step = state['global_step']
+    else:
+        print('No model to preload, starting from scratch')
+
+    loss_fn = nn.CrossEntropyLoss(ignore_index=tokenizer_src.token_to_id(
+        '[PAD]'), label_smoothing=0.1).to(device)
+
+    for epoch in range(initial_epoch, config['num_epochs']):
+        torch.cuda.empty_cache()
+        model.train()
+        batch_iterator = tqdm(
+            train_dataloader, desc=f"Processing Epoch {epoch:02d}")
+        for batch in batch_iterator:
+
+            encoder_input = batch['encoder_input'].to(device)  # (b, seq_len)
+            decoder_input = batch['decoder_input'].to(device)  # (B, seq_len)
+            encoder_mask = batch['encoder_mask'].to(
+                device)  # (B, 1, 1, seq_len)
+            decoder_mask = batch['decoder_mask'].to(
+                device)  # (B, 1, seq_len, seq_len)
+
+            # Run the tensors through the encoder, decoder and the projection layer
+            encoder_output = model.encode(
+                encoder_input, encoder_mask)  # (B, seq_len, d_model)
+            decoder_output = model.decode(
+                encoder_output, encoder_mask, decoder_input, decoder_mask)  # (B, seq_len, d_model)
+            # (B, seq_len, vocab_size)
+            proj_output = model.project(decoder_output)
+
+            # Compare the output with the label
+            label = batch['label'].to(device)  # (B, seq_len)
+
+            # Compute the loss using a simple cross entropy
+            loss = loss_fn(
+                proj_output.view(-1, tokenizer_tgt.get_vocab_size()), label.view(-1))
+            batch_iterator.set_postfix({"loss": f"{loss.item():6.3f}"})
+
+            # Log the loss
+            writer.add_scalar('train loss', loss.item(), global_step)
+            writer.flush()
+
+            # Backpropagate the loss
+            loss.backward()
+
+            # Update the weights
+            optimizer.step()
+            optimizer.zero_grad(set_to_none=True)
+
+            global_step += 1
+
+        # Run validation at the end of every epoch
+        run_validation(model, val_dataloader, tokenizer_src, tokenizer_tgt,
+                       config['seq_len'], device, lambda msg: batch_iterator.write(msg), global_step, writer)
+
+        # Save the model at the end of every epoch
+        model_filename = get_weights_file_path(config, f"{epoch:02d}")
+        torch.save({
+            'epoch': epoch,
+            'model_state_dict': model.state_dict(),
+            'optimizer_state_dict': optimizer.state_dict(),
+            'global_step': global_step
+        }, model_filename)
+"""
